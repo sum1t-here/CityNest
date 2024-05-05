a0ee89e0cce0c18ccd29032b4fa3d1e1a97424f1
sumit <mazumdarsumit3@gmail.com>
2024-05-05 13:02:18 +0530

    create update listing api route

--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from '../controllers/listing.controller.js';
 import { validUser } from '../utils/validUser.js';
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.post('/create', validUser, createListing);
 router.delete('/delete/:id', validUser, deleteListing);
+router.post('//update/:id', validUser, updateListing);
 
 export default router;
