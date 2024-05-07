7f4e6a670890052386d317cf384e9753da908931
sumit <mazumdarsumit3@gmail.com>
2024-05-08 03:12:26 +0530

    Complete Search functionality

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -2,5 +2,6 @@ import Header from './Header';
 import Oauth from './Oauth';
 import PrivateRoute from './PrivateRoute';
 import Contact from './Contact';
+import ListingItem from './ListingItem';
 
-export { Header, Oauth, PrivateRoute, Contact };
+export { Header, Oauth, PrivateRoute, Contact, ListingItem };
--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ListingItem } from '../Components';
 
 function Search() {
   const [sidebarData, setSidebarData] = useState({
@@ -11,6 +12,8 @@ function Search() {
     sort: 'createdAt',
     order: 'desc',
   });
+
+  const [showMore, setShowMore] = useState(false);
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
@@ -46,9 +49,15 @@ function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
+      if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -104,6 +113,20 @@ function Search() {
     urlParams.set('order', sidebarData.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
   return (
     <div className='flex flex-col md:flex-row'>
@@ -207,10 +230,34 @@ function Search() {
           </button>
         </form>
       </div>
-      <div className=''>
+      <div className='flex-1'>
         <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
           Listing results:
         </h1>
+        <div className='p-7 flex flex-wrap gap-4'>
+          {!loading && listings.length === 0 && (
+            <p className='text-xl text-slate-700'>No listing found!</p>
+          )}
+          {loading && (
+            <p className='text-xl text-slate-700 text-center w-full'>
+              Loading...
+            </p>
+          )}
+
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
