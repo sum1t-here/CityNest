2f8e2fcbef17e9ed70198121d798a59712fcdf6b
sumit <mazumdarsumit3@gmail.com>
2024-05-08 04:01:45 +0530

    changed the port config

--- api/index.js
+++ api/index.js
@@ -26,7 +26,9 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   console.log(`Server is running on port 3000`);
 });
 
