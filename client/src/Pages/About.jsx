a758d88fe6c2293778e960e1bd93eb8fed475074
sumit <mazumdarsumit3@gmail.com>
2024-05-08 03:32:02 +0530

    Complete about page

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,7 +1,36 @@
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About City Nest
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        City Nest is a premier real estate agency specializing in assisting
+        clients with buying, selling, and renting properties in vibrant urban
+        environments. Our dedicated team of professionals is committed to
+        delivering exceptional service and ensuring a seamless experience for
+        our clients.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our mission at City Nest is to empower our clients to make informed
+        decisions about their real estate investments. With our in-depth market
+        knowledge and personalized approach, we strive to exceed expectations
+        and build long-lasting relationships with our clients.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        At City Nest, we understand the unique dynamics of urban living and the
+        importance of finding the perfect home or investment property in the
+        heart of the city. Whether you're searching for a trendy loft, a cozy
+        apartment, or a prime commercial space, we're here to help you find your
+        ideal urban sanctuary.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Our team is dedicated to providing unparalleled customer service and
+        leveraging the latest technology and marketing strategies to achieve the
+        best possible results for our clients. We are passionate about urban
+        real estate and committed to helping you navigate the city's diverse and
+        dynamic property landscape.
+      </p>
     </div>
   );
 }
